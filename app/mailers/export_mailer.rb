fcef8e93444a5f4ea4c9af76bf5b359ab88dd49c
Birol Senturk <birol_senturk@brown.edu>
2019-05-22 10:00:39 -0400

    working on filtering

--- app/mailers/export_mailer.rb
+++ app/mailers/export_mailer.rb
@@ -1,9 +1,10 @@
 class ExportMailer < ApplicationMailer
   default from: 'support@srdrPLUS.com'
 
-  def notify_simple_export_completion(current_user_id, project_id)
-    @user    = User.find current_user_id
-    @project = Project.find project_id
+  def notify_simple_export_completion(exported_file_id)
+    @exported_file = ExportedFile.find exported_file_id
+    @user    = @exported_file.user
+    @project = @exported_file.project
     mail(to: @user.email, subject: 'You have a notification from srdrPLUS')
   end
 end
